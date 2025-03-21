6ee08640314565980afa4eb0462a2c820c952232
Matteo Collina <hello@matteocollina.com>
2025-03-21 14:36:41 +0100

    updated .gitignore and passing test
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- test/utils/crc32.test.ts
+++ test/utils/crc32.test.ts
@@ -1,7 +1,7 @@
 import BufferList from 'bl'
 import { deepStrictEqual } from 'node:assert'
 import test from 'node:test'
-import { crc32c } from '../../src/utils/crc32c.ts'
+import { crc32c } from '../../src/protocol/crc32c.ts'
 
 // Samples copied from https://github.com/tulios/kafkajs/blob/55b0b416308b9e597a5a6b97b0a6fd6b846255dc/src/protocol/recordBatch/crc32c/fixtures/samples.js
 const samples = [

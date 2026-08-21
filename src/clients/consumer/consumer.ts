c9e8bdab6cd2bcd69287b3e00c2d9c575c8d45fe
W1sdomPanda <vlad1slavskrygun@gmail.com>
2026-08-21 13:40:50 +0300

    feature: fix lint
    Signed-off-by: W1sdomPanda <vlad1slavskrygun@gmail.com>

--- src/clients/consumer/consumer.ts
+++ src/clients/consumer/consumer.ts
@@ -1960,8 +1960,8 @@ export class Consumer<Key = Buffer, Value = Buffer, HeaderKey = Buffer, HeaderVa
   }
 
   #performJoinGroup (
-    options: Required<GroupOptions>, 
-    callback: CallbackWithPromise<string>, 
+    options: Required<GroupOptions>,
+    callback: CallbackWithPromise<string>,
     rejoinAttempts: number = 0
   ): void {
     if (!this.#membershipActive) {
@@ -2038,7 +2038,7 @@ export class Consumer<Key = Buffer, Value = Buffer, HeaderKey = Buffer, HeaderVa
             }
 
             if (error) {
-            if (this.#getRejoinError(error) && rejoinAttempts < (this[kOptions].retries as number)) {
+              if (this.#getRejoinError(error) && rejoinAttempts < (this[kOptions].retries as number)) {
                 this.#performJoinGroup(options, callback, rejoinAttempts + 1)
                 return
               }
